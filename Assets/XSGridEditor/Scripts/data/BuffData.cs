143f4492a88e0e87f349fbc2e4855ad6317e7217
wuneng117 <540669277@qq.com>
2022-10-09 13:02:26 +0800

    feat: 1111

--- Assets/XSGridEditor/Scripts/data/BuffData.cs
+++ Assets/XSGridEditor/Scripts/data/BuffData.cs
@@ -1,71 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace XSSLG
 {
+    [Serializable]
     public partial class BuffData : BaseData
     {
+
         /// <summary>类型</summary>
-        [System.ComponentModel.DisplayName("类型")]
-        public int Type { get; private set; }
+        [SerializeField]
+        public int Type { get; set; }
 
         /// <summary>常用效果</summary>
-        [System.ComponentModel.DisplayName("常用效果")]
-        public List<global::XSSLG.SkillEffectStruct> EffectArray { get; private set; }
-
-        /// <summary>Stat</summary>
-        [System.ComponentModel.DisplayName("Stat")]
-        public long StatId { get; private set; }
+        [SerializeField]
+        public List<SkillEffectStruct> EffectArray { get; protected set; } = new List<SkillEffectStruct>();
 
         /// <summary>最大层数</summary>
-        [System.ComponentModel.DisplayName("最大层数")]
-        public int MaxCount { get; private set; }
+        [SerializeField]
+        protected int maxCount;
+        public int MaxCount { get => maxCount; set => maxCount = value; }
 
         /// <summary>起始层数</summary>
-        [System.ComponentModel.DisplayName("起始层数")]
-        public int InitCount { get; private set; }
+        [SerializeField]
+        protected int initCount;
+        public int InitCount { get => initCount; set => initCount = value; }
 
         /// <summary>Buff结束触发器Id</summary>
-        [System.ComponentModel.DisplayName("Buff结束触发器Id")]
-        public int FinishTriggerId { get; private set; }
+        [SerializeField]
+        public int FinishTriggerId { get; set; }
 
         /// <summary>持续时间</summary>
-        [System.ComponentModel.DisplayName("持续时间")]
-        public int Duration { get; private set; }
+        [SerializeField]
+        protected int duration;
+        public int Duration { get => duration; set => duration = value; }
 
         /// <summary>能否叠加</summary>
-        [System.ComponentModel.DisplayName("能否叠加")]
-        public bool CanStack { get; private set; }
+        [SerializeField]
+        protected bool canStack;
+        public bool CanStack { get => canStack; set => canStack = value; }
 
         /// <summary>效果随层数变动</summary>
-        [System.ComponentModel.DisplayName("效果随层数变动")]
-        public int CanFloor { get; private set; }
+        [SerializeField]
+        protected int canFloor;
+        public int CanFloor { get => canFloor; set => canFloor = value; }
 
         /// <summary>自定义数值数组</summary>
-        [System.ComponentModel.DisplayName("自定义数值数组")]
-        public List<global::XSSLG.PropStruct> PropArray { get; private set; }
+        [SerializeField]
+        public List<PropStruct> PropArray { get; protected set; } = new List<PropStruct>();
 
         /// <summary>是否debuff</summary>
-        [System.ComponentModel.DisplayName("是否debuff")]
-        public bool IsDebuff { get; private set; }
+        [SerializeField]
+        protected bool isDebuff;
+        public bool IsDebuff { get => isDebuff; set => isDebuff = value; }
 
-
-        public static Int32 PID { get { return 0; } }
-
-        // TODO 没有实现
-        public StatData StatData { get; private set; }
+        [SerializeField]
+        public StatData StatData { get; set; }
     }
-
-    public partial class BuffDataArray
-    {
-        public List<long> Keys { get; private set; }
-
-        public List<global::XSSLG.BuffData> Items { get; private set; }
-
-        public string TableHash { get; private set; }
-
-
-        public static Int32 PID { get { return 0;}}
-    }
-
 }
--- Assets/XSGridEditor/Scripts/data/common.cs
+++ Assets/XSGridEditor/Scripts/data/common.cs
@@ -5,66 +5,32 @@ using UnityEngine;
 
 namespace XSSLG
 {
-    public partial class CommonTypeHolder
-    {
-        public global::XSSLG.CompareType Hold1 { get; private set; }
-
-        public global::XSSLG.PropStruct Hold2 { get; private set; }
-
-        public global::XSSLG.SkillEffectStruct Hold3 { get; private set; }
-
-        public global::XSSLG.SkillEffectType Hold4 { get; private set; }
-
-        public global::XSSLG.TargetType Hold5 { get; private set; }
-
-        public global::XSSLG.TechniqueLevel Hold6 { get; private set; }
-
-        public XSDefine.TechniqueType Hold7 { get; private set; }
-
-        public global::XSSLG.WeaponType Hold8 { get; private set; }
-
-
-        public static Int32 PID { get { return 0;}}
-    }
-
-    public partial class PropStruct
+    [Serializable]
+    public class PropStruct
     {
         /// <summary>填需要用到的StatData里列的名字</summary>
-        [System.ComponentModel.DisplayName("填需要用到的StatData里列的名字")]
-        public string Type { get; private set; }
+        [SerializeField]
+        public string Type { get; set; }
 
         /// <summary>Type的百分比系数</summary>
-        [System.ComponentModel.DisplayName("Type的百分比系数")]
-        public float Percent { get; private set; }
+        [SerializeField]
+        public float Percent { get; set; }
 
         /// <summary>加上Type的百分比就是最终数值</summary>
-        [System.ComponentModel.DisplayName("加上Type的百分比就是最终数值")]
-        public float Prop { get; private set; }
-
-        /// <summary>备注</summary>
-        [System.ComponentModel.DisplayName("备注")]
-        public string Desc { get; private set; }
-
-
-        public static Int32 PID { get { return 0;}}
+        [SerializeField]
+        public float Prop { get; set; }
     }
 
-    public partial class SkillEffectStruct
+    [Serializable]
+    public class SkillEffectStruct
     {
         /// <summary>效果类型</summary>
-        [System.ComponentModel.DisplayName("效果类型")]
-        public global::XSSLG.SkillEffectType Type { get; private set; }
+        [SerializeField]
+        public SkillEffectType Type { get; set; }
 
         /// <summary>数值</summary>
-        [System.ComponentModel.DisplayName("数值")]
-        public float Prop { get; private set; }
-
-        /// <summary>说明</summary>
-        [System.ComponentModel.DisplayName("说明")]
-        public string Desc { get; private set; }
-
-
-        public static Int32 PID { get { return 0;}}
+        [SerializeField]
+        public float Prop { get; set; }
     }
 
     [Serializable]
@@ -73,13 +39,11 @@ namespace XSSLG
         /// <summary>技能类型</summary>
         [SerializeField]
         protected XSDefine.TechniqueType type;
-        [System.ComponentModel.DisplayName("技能类型")]
         public XSDefine.TechniqueType Type { get => type; set => type = value; }
 
         /// <summary>等级</summary>
         [SerializeField]
         protected int lv;
-        [System.ComponentModel.DisplayName("等级")]
         public int Lv { get => lv; set => lv = value; }
     }
 
@@ -87,14 +51,12 @@ namespace XSSLG
     {
         None = 0,
         /// <summary>相等</summary>
-        [System.ComponentModel.Description("相等")]
         Equal = 1,
         /// <summary>小于</summary>
-        [System.ComponentModel.Description("小于")]
         LessThan = 2,
         /// <summary>大于</summary>
-        [System.ComponentModel.Description("大于")]
         MoreThan = 3,
+        Max
     }
 
 
@@ -102,11 +64,10 @@ namespace XSSLG
     {
         None = 0,
         /// <summary>物理免伤百分比</summary>
-        [System.ComponentModel.Description("物理免伤百分比")]
         PhyImmunityFactor = 1,
         /// <summary>物理伤害百分比</summary>
-        [System.ComponentModel.Description("物理伤害百分比")]
         PhyDamageFactor = 2,
+        Max
     }
 
 
@@ -114,27 +75,22 @@ namespace XSSLG
     {
         Self = 0,
         Enemy = 0,
+        Max
     }
 
     public enum WeaponType
     {
         None = 0,
         /// <summary>剑</summary>
-        [System.ComponentModel.Description("剑")]
         Sword = 1,
         /// <summary>枪</summary>
-        [System.ComponentModel.Description("枪")]
         Lance = 2,
         /// <summary>斧</summary>
-        [System.ComponentModel.Description("斧")]
         Axe = 3,
         /// <summary>弓</summary>
-        [System.ComponentModel.Description("弓")]
         Bow = 4,
         /// <summary>拳套</summary>
-        [System.ComponentModel.Description("拳套")]
         Brawl = 5,
+        Max
     }
-
-
 }
